b7ad57fdc5527f362653564d0a4fe438b6005ac9
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2024-02-22 12:25:42 +0600

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void circle(int radious)
+{
+    cout<<"meter"<<2*3.1416*radious<<endl;
+    cout<<"area"<<3.1416*radious*radious<<endl;
+}
+int main()
+{
+    circle(4);
+    return 0;
+}
