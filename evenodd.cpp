94c1ce7558a8e609acdf076bbb179eeb3cef1deb
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2024-02-22 12:19:26 +0600

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void co(int x)
+{
+    if(x%2==0)
+    {
+        cout<<"even"<<endl;
+    }
+    else 
+    {
+        cout<<"odd"<<endl;
+    }
+}
+int main ()
+{
+    co(4);
+    co(5);
+    return 0;
+}
